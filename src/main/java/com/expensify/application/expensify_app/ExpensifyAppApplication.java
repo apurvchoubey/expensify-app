54851c50d8e5e2b3d61bbc661ed7316de0d3a228
unknown <choubeyapurv@gmail.com>
2024-06-13 19:31:35 +0530

    initial Commit

---
+++ src/main/java/com/expensify/application/expensify_app/ExpensifyAppApplication.java
@@ -0,0 +1,13 @@
+package com.expensify.application.expensify_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensifyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensifyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expensify/application/expensify_app/ExpensifyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expensify.application.expensify_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensifyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
